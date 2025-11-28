ff548fd5568843857ccfff91274316a290c9537a
GilOsterweil <gil.osterweil@gmail.com>
2025-11-28 19:45:35 +0200

    first

---
+++ mysql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO users (name) VALUES ('Gil'), ('Admin'), ('Test');
